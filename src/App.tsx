583ebb2ad60f3612594d967925d2d0516958309f
Valentyn Bondarenko <murica.it@gmail.com>
2025-04-13 02:22:16 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoutes } from './types/AppRoutes';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path={AppRoutes.LOGIN} element={<Login />} />
+                <Route path={AppRoutes.REGISTER} element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
